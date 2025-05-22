55374fe334925ac22f3ad7fec646040b609b3912
ombuijabali <simion7ombui@gmail.com>
2025-05-22 11:47:56 +0300

    updated setting.py

--- core/settings.py
+++ core/settings.py
@@ -303,10 +303,11 @@ ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Cloudinary settings
+CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
-
+}
 
 # Media files configuration
 MEDIA_URL = '/media/'
